4cd0ee3d89138ade57a349409034cf6bcf4920a2
arantesvictorl <victorlsarantes@gmail.com>
2025-10-20 12:51:08 -0300

    Update vit_img_size and vit_model_type in VLMConfig to enhance model capabilities for larger input images.

--- models/config.py
+++ models/config.py
@@ -6,13 +6,13 @@ class VLMConfig:
     vit_hidden_dim: int = 768
     vit_inter_dim: int = 4 * vit_hidden_dim
     vit_patch_size: int = 16
-    vit_img_size: int = 224
+    vit_img_size: int = 512
     vit_n_heads: int = 12
     vit_dropout: float = 0.0
     vit_n_blocks: int = 12
     vit_ln_eps: float = 1e-6
     vit_cls_flag: bool = False
-    vit_model_type: str = 'google/siglip-base-patch16-224'
+    vit_model_type: str = 'google/siglip2-base-patch16-512'
 
     lm_hidden_dim: int = 960
     lm_inter_dim: int = 2560
